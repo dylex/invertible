0ad2952d134736e2e4eb83439cd5ad153f998d7c
Dylan Simon <dylan@dylex.net>
2016-04-07 03:41:34 -0400

    Expose Ord and re-sort modules

--- Data/Isomorphism.hs
+++ Data/Isomorphism.hs
@@ -11,29 +11,31 @@ module Data.Isomorphism
   , module Data.Isomorphism.TH
   , module Data.Isomorphism.Prelude
   , module Data.Isomorphism.Arrow
+  , module Data.Isomorphism.Bits
   , module Data.Isomorphism.Bool
-  , module Data.Isomorphism.Either
-  , module Data.Isomorphism.Tuple
-  , module Data.Isomorphism.List
-  , module Data.Isomorphism.Maybe
-  , module Data.Isomorphism.Function
-  , module Data.Isomorphism.Functor
   , module Data.Isomorphism.Coerce
   , module Data.Isomorphism.Complex
-  , module Data.Isomorphism.Bits
+  , module Data.Isomorphism.Either
+  , module Data.Isomorphism.Function
+  , module Data.Isomorphism.Functor
+  , module Data.Isomorphism.List
+  , module Data.Isomorphism.Maybe
+  , module Data.Isomorphism.Ord
+  , module Data.Isomorphism.Tuple
   ) where
 
 import Data.Isomorphism.Type
 import Data.Isomorphism.TH
 import Data.Isomorphism.Prelude
 import Data.Isomorphism.Arrow
+import Data.Isomorphism.Bits
 import Data.Isomorphism.Bool
-import Data.Isomorphism.Either
-import Data.Isomorphism.Tuple
-import Data.Isomorphism.List
-import Data.Isomorphism.Maybe
-import Data.Isomorphism.Function
-import Data.Isomorphism.Functor
 import Data.Isomorphism.Coerce
 import Data.Isomorphism.Complex
-import Data.Isomorphism.Bits
+import Data.Isomorphism.Either
+import Data.Isomorphism.Function
+import Data.Isomorphism.Functor
+import Data.Isomorphism.List
+import Data.Isomorphism.Maybe
+import Data.Isomorphism.Ord
+import Data.Isomorphism.Tuple
