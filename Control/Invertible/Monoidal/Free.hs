5548a290aa48821d5d7384d2423547072011ab96
Dylan Simon <dylan@dylex.net>
2016-05-14 20:59:56 -0400

    Generalize and improve some Free functions

--- Control/Invertible/Monoidal/Free.hs
+++ Control/Invertible/Monoidal/Free.hs
@@ -4,14 +4,17 @@
 -- This can provide a simple basis for things like invertible parsers.
 module Control.Invertible.Monoidal.Free
   ( Free(..)
-  , produceList
-  , consumeList
-  , reverseList
+  , foldFree
+  , produceFree
+  , runFree
+  , parseFree
+  , reverseFree
   ) where
 
-import Control.Applicative ((<|>))
+import Control.Applicative (Alternative, (<|>))
 import Control.Monad (MonadPlus(..))
 import Control.Monad.Trans.State (StateT(..))
+import Data.Monoid ((<>), Alt(..))
 
 import Control.Invertible.Monoidal
 import qualified Data.Invertible as I
@@ -36,32 +39,45 @@ instance Monoidal (Free f) where
 instance MonoidalAlt (Free f) where
   (>|<) = Choose
 
--- |Given a way to extract @b@ elements from any @f a@, use a 'Free' applied to a value to produce a list of @b@ elements by converting '>*<' to '++'.
-produceList :: (forall a' . f a' -> a' -> b) -> Free f a -> a -> [b]
-produceList _ Empty () = []
-produceList t (Transform f p) a = produceList t p $ I.biFrom f a
-produceList t (Join p q) (a, b) = produceList t p a ++ produceList t q b
-produceList t (Choose p _) (Left a) = produceList t p a
-produceList t (Choose _ p) (Right a) = produceList t p a
-produceList t (Free x) a = [t x a]
+-- |Transform the type constructor within a 'Free'.
+mapFree :: (forall a' . f a' -> m a') -> Free f a -> Free m a
+mapFree _ Empty = Empty
+mapFree t (Transform f p) = Transform f $ mapFree t p
+mapFree t (Join p q) = Join (mapFree t p) (mapFree t q)
+mapFree t (Choose p q) = Choose (mapFree t p) (mapFree t q)
+mapFree t (Free x) = Free (t x)
+
+-- |Given a way to extract a @b@ from any @f a@, use a 'Free' applied to a value to produce a @b@ by converting '>*<' to '<>'.
+foldFree :: Monoid b => (forall a' . f a' -> a' -> b) -> Free f a -> a -> b
+foldFree _ Empty () = mempty
+foldFree t (Transform f p) a = foldFree t p $ I.biFrom f a
+foldFree t (Join p q) (a, b) = foldFree t p a <> foldFree t q b
+foldFree t (Choose p _) (Left a) = foldFree t p a
+foldFree t (Choose _ p) (Right a) = foldFree t p a
+foldFree t (Free x) a = t x a
+
+-- |'foldFree' over Alternative rather than Monoid.
+produceFree :: Alternative m => (forall a' . f a' -> a' -> b) -> Free f a -> a -> m b
+produceFree t f = getAlt . foldFree (\x a -> Alt $ pure $ t x a) f
+
+-- |Evaluate a 'Free' into an underlying 'MonadPlus', by evaluating '>|<' with '<|>'.
+runFree :: MonadPlus f => Free f a -> f a
+runFree Empty = return ()
+runFree (Transform f p) = I.biTo f <$> runFree p
+runFree (Join p q) = (,) <$> runFree p <*> runFree q
+runFree (Choose p q) = Left <$> runFree p <|> Right <$> runFree q
+runFree (Free x) = x
 
 -- |Given a way to convert @b@ elements into any @f a@, use a 'Free' to parse a list of @b@ elements into a value.
--- This is the inverse of 'produceList', provided the given conversions are themselves inverses.
-consumeList :: MonadPlus m => (forall a' . f a' -> b -> m a') -> Free f a -> StateT [b] m a
-consumeList _ Empty = return ()
-consumeList t (Transform f p) = I.biTo f <$> consumeList t p
-consumeList t (Join p q) = (,) <$> consumeList t p <*> consumeList t q
-consumeList t (Choose p q) = Left <$> consumeList t p <|> Right <$> consumeList t q
-consumeList t (Free x) = StateT f where
-  f (a:l) = (, l) <$> t x a
-  f [] = mzero
+-- This just uses 'I.uncons' with 'runFree', and is the inverse of 'produceFree', provided the given conversions are themselves inverses.
+parseFree :: MonadPlus m => (forall a' . f a' -> b -> m a') -> Free f a -> [b] -> m (a, [b])
+parseFree t = runStateT . runFree . mapFree (\x -> StateT $ \l -> case l of { (a:r) -> (, r) <$> t x a ; [] -> mzero })
 
--- |Flip the effective order of each '>*<' operation in a 'Free', so that 'produceList' or 'consumeList' produce or consume reversed lists.
--- This also lets you reverse the order of parsing in 'consumeList'.
+-- |Flip the effective order of each '>*<' operation in a 'Free', so that processing is done in the reverse order.
 -- It probably goes without saying, but applying this to an infinite structure, such as those produced by 'manyI', will not terminate.
-reverseList :: Free f a -> Free f a
-reverseList (Transform f (Join p q)) = Transform (f I.. I.swap) $ Join (reverseList q) (reverseList p)
-reverseList (Transform f p) = Transform f $ reverseList p
-reverseList (Join p q) = Transform I.swap $ Join (reverseList q) (reverseList p)
-reverseList (Choose p q) = Choose (reverseList p) (reverseList q)
-reverseList p = p
+reverseFree :: Free f a -> Free f a
+reverseFree (Transform f (Join p q)) = Transform (f I.. I.swap) $ Join (reverseFree q) (reverseFree p)
+reverseFree (Transform f p) = Transform f $ reverseFree p
+reverseFree (Join p q) = Transform I.swap $ Join (reverseFree q) (reverseFree p)
+reverseFree (Choose p q) = Choose (reverseFree p) (reverseFree q)
+reverseFree p = p
